2d648fda74c3df3cd1101a66a5ae98ae5af6a5f3
FerasGIT <ferasny@gmail.com>
2025-08-07 23:22:54 +0300

    added register funcation and it works is intended

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,5 @@ const router = require('express').Router()
 const authController = require('../controllers/auth')
 
 router.post('/signIn', authController.auth_sigin_post)
+router.post('/register', authController.auth_register_post)
 module.exports = router
