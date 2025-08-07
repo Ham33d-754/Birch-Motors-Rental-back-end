46766e398f5a60cdce3086000ef87daea9d98ef3
FerasGIT <ferasny@gmail.com>
2025-08-08 00:02:06 +0300

    fixed issues with token session

--- middleware/index.js
+++ middleware/index.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 exports.verfiyToken = (req, res, next) => {
   const token = req.headers['authorization'].split(' ')[1]
   if (token) {
-    res.local.token = token
+    res.locals.token = token
     const payload = jwt.verify(token, process.env.APP_SECRET)
     if (payload) {
       res.locals.payload = payload
-      next()
+      return next()
     }
   }
   res.status(401).send({ msg: 'unauthorized' })
